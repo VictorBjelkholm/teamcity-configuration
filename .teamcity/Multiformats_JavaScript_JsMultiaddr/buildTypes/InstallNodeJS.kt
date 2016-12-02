65afc71283176f362f31e8a590db6f2d474149b5
Victor Bjelkholm <victorbjelkholm@gmail.com>
2016-12-02 14:46:01 +0100

    Rename where template comes from

--- .teamcity/Multiformats_JavaScript_JsMultiaddr/buildTypes/InstallNodeJS.kt
+++ .teamcity/Multiformats_JavaScript_JsMultiaddr/buildTypes/InstallNodeJS.kt
@@ -3,7 +3,7 @@ package Multiformats_JavaScript_JsMultiaddr.buildTypes
 import jetbrains.buildServer.configs.kotlin.v10.*
 
 object InstallNodeJS : BuildType({
-    template(_Root.buildTypes.Multiformats_JavaScript_JsMultiaddr_InstallNodejs6debian)
+    template(_Root.buildTypes.InstallNodejs6debian)
     uuid = "cac3f408-a294-472f-ba54-468bb369ab55"
     extId = "Multiformats_JavaScript_JsMultiaddr_InstallNodeJS"
     name = "Install NodeJS"
--- .teamcity/Multiformats_JavaScript_JsMultihashingAsync/buildTypes/InstallNodeJS.kt
+++ .teamcity/Multiformats_JavaScript_JsMultihashingAsync/buildTypes/InstallNodeJS.kt
@@ -3,7 +3,7 @@ package Multiformats_JavaScript_JsMultihashingAsync.buildTypes
 import jetbrains.buildServer.configs.kotlin.v10.*
 
 object InstallNodeJS : BuildType({
-    template(_Root.buildTypes.Multiformats_JavaScript_JsMultiaddr_InstallNodejs6debian)
+    template(_Root.buildTypes.InstallNodejs6debian)
     uuid = "5d83d3b8-498c-44b9-8275-481d97721b19"
     extId = "Multiformats_JavaScript_JsMultihashingAsync_InstallNodeJS"
     name = "Install NodeJS"
